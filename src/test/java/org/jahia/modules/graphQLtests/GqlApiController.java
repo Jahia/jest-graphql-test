c39eb652feacc3938f9d6d42dd1dd60069a38fb1
astritademi <aademi@jahia.com>
2019-02-05 13:23:08 -0500

    Removed node-modules and .idea folders and added them to .gitignore

--- src/test/java/org/jahia/modules/graphQLtests/GqlApiController.java
+++ src/test/java/org/jahia/modules/graphQLtests/GqlApiController.java
@@ -1,5 +1,6 @@
 package org.jahia.modules.graphQLtests;
 
+import graphql.servlet.OsgiGraphQLServlet;
 import net.htmlparser.jericho.Attribute;
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.Source;
@@ -9,16 +10,25 @@ import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.jahia.modules.tests.core.ModuleTest;
+import org.jahia.osgi.BundleUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
+import javax.servlet.Servlet;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 /**
  * Created by parveer on 2019-01-15.
@@ -29,29 +39,34 @@ public class GqlApiController extends ModuleTest {
     private HttpClient httpClient;
     GetMethod get = new GetMethod(getsdlreporttoolPath());
 
+    private static OsgiGraphQLServlet servlet;
 
-    @BeforeSuite
-    public void importDigitall(){
-        getDriver().navigate().to(getPath("/cms/admin/default/en/settings.webProjectSettings.html"));
-        switchToDXAdminFrame();
-
-        WebElement site = findByXpath("//a[contains(.,'Digitall')]");
-        if(site == null){
-            performSelectDropdownVisibleText(findByName("selectedPrepackagedSite"), "Digitall Prepackaged Demo Website");
-            clickOn(findByName("importPrepackaged"));
-            clickOn(findByXpath("//button[contains(.,'Import')]"));
-
-            waitForElementToBeInvisible(By.xpath("//div[contains(., 'Work in progress, please wait...')]"));
-            sleepMultipleTime(60);
-            waitForWorkToEnd();
-
-            Assert.assertNotNull(waitForElementToBeVisible(findByXpath("//a[contains(.,'Digitall')]")), "Failed to import Digitall site");
-        }
-        else {
-            Assert.assertNotNull(waitForElementToBeVisible(findByXpath("//a[contains(.,'Digitall')]")), "Failed to import Digitall site");
-            logger.info("Site already exists");
-        }
+    public static void setServlet(OsgiGraphQLServlet servlet) {
+        GqlApiController.servlet = servlet;
     }
+
+//    @BeforeSuite
+//    public void importDigitall(){
+//        getDriver().navigate().to(getPath("/cms/admin/default/en/settings.webProjectSettings.html"));
+//        switchToDXAdminFrame();
+//
+//        WebElement site = findByXpath("//a[contains(.,'Digitall')]");
+//        if(site == null){
+//            performSelectDropdownVisibleText(findByName("selectedPrepackagedSite"), "Digitall Prepackaged Demo Website");
+//            clickOn(findByName("importPrepackaged"));
+//            clickOn(findByXpath("//button[contains(.,'Import')]"));
+//
+//            waitForElementToBeInvisible(By.xpath("//div[contains(., 'Work in progress, please wait...')]"));
+//            sleepMultipleTime(60);
+//            waitForWorkToEnd();
+//
+//            Assert.assertNotNull(waitForElementToBeVisible(findByXpath("//a[contains(.,'Digitall')]")), "Failed to import Digitall site");
+//        }
+//        else {
+//            Assert.assertNotNull(waitForElementToBeVisible(findByXpath("//a[contains(.,'Digitall')]")), "Failed to import Digitall site");
+//            logger.info("Site already exists");
+//        }
+//    }
 
     @BeforeClass
     public void setHttpClient(){
@@ -59,24 +74,24 @@ public class GqlApiController extends ModuleTest {
         httpClient.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("jahia", "password"));
     }
 
-    //@AfterSuite
-    public void deleteSite(){
-        getDriver().navigate().to(getPath("/cms/admin/default/en/settings.webProjectSettings.html"));
-
-        switchToDXAdminFrame();
-
-        List<WebElement> sites = findElementsByXpath("//a[@title='Delete']");
-        for(WebElement deleteButton:sites) {
-            int i=0;
-            clickOn(sites.get(i));
-
-            clickOn(deleteButton);
-            waitForElementToBeInvisible(By.xpath("//div[contains(., 'Work in progress, please wait...')]"));
-            sleepMultipleTime(60);
-            waitForWorkToEnd();
-        }
-
-    }
+//    @AfterSuite
+//    public void deleteSite(){
+//        getDriver().navigate().to(getPath("/cms/admin/default/en/settings.webProjectSettings.html"));
+//
+//        switchToDXAdminFrame();
+//
+//        List<WebElement> sites = findElementsByXpath("//a[@title='Delete']");
+//        for(WebElement deleteButton:sites) {
+//            int i=0;
+//            clickOn(sites.get(i));
+//
+//            clickOn(deleteButton);
+//            waitForElementToBeInvisible(By.xpath("//div[contains(., 'Work in progress, please wait...')]"));
+//            sleepMultipleTime(60);
+//            waitForWorkToEnd();
+//        }
+//
+//    }
 
     private void switchToDXAdminFrame() {
         driver.switchTo().frame(findByXpath("//iframe[contains(@src,'/cms/adminframe/default/en/settings.webProjectSettings.html')]"));
@@ -125,5 +140,26 @@ public class GqlApiController extends ModuleTest {
         System.out.println("Actual status is : " + Actualstatus);
         Assert.assertEquals(Actualstatus, expectedStatus, "Module status has error");
     }
+
+    protected static JSONObject executeQuery(String query) throws JSONException {
+        String result = servlet.executeQuery(query);
+        if (result != null) {
+            if (result.contains("Validation error")) {
+                logger.error("Validation error {} for query: {}", result, query);
+            } else if (result.contains("Invalid Syntax")) {
+                logger.error("Invalid Syntax\" {} for query: {}", result, query);
+            }
+        }
+        return new JSONObject(result);
+    }
+
+//    protected static Map<String, JSONObject> toItemByKeyMap(String key, JSONArray items) throws JSONException {
+//        HashMap<String, JSONObject> itemByName = new HashMap<JSONArray>(items.length());
+//        for (int i = 0; i < items.length(); i++) {
+//            JSONObject item = items.getJSONObject(i);
+//            itemByName.put(item.getString(key), item);
+//        }
+//        return itemByName;
+//    }
 
 }

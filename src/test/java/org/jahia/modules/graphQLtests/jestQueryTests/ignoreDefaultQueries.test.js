0f57cf8c02965ebd154de2b008e5ccaa02aed2ba
astritademi <aademi@jahia.com>
2019-02-08 07:40:54 -0500

    BACKLOG-9615 - added ignoreDefaultQueries test              - renamed byall.query.test.js to byAllQuery.test.js

---
+++ src/test/java/org/jahia/modules/graphQLtests/jestQueryTests/ignoreDefaultQueries.test.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+//server
+const server = 'http://localhost:8080/modules/graphql';
+
+//headers config
+const axiosConf = {
+    headers: {
+        authorization: 'Basic cm9vdDpyb290MTIzNA=='
+    }
+};
+
+describe('Graphql Query Tests - Ignore Default Queries', () => {
+
+    test('allNewsSDL query test: should retrieve an error', async () => {
+        const response = await axios.post(server, {
+            query:
+            `{
+                allNewsSDL (language:"en") {
+                    title
+                }
+            }`
+        }, axiosConf);
+
+        const { data } = response;
+
+        expect(data.errors[0]).toHaveProperty("description", "Field 'allNewsSDL' in type 'Query' is undefined")
+    });
+
+    test('newsSDLById query test: should retrieve an error', async () => {
+        const response = await axios.post(server, {
+            query:
+                `{
+                newsSDLById (id: "c81d50fd-7807-46ae-a4e1-96b5ba9eb2e5") {
+                    title
+                }
+            }`
+        }, axiosConf);
+
+        const { data } = response;
+
+        expect(data.errors[0]).toHaveProperty("description", "Field 'newsSDLById' in type 'Query' is undefined");
+    });
+
+
+    test('newsSDLByPath query test: should retrieve an error', async () => {
+        const response = await axios.post(server, {
+            query:
+                `{
+                newsSDLByPath (path: "/sites/digitall/home/about/history/area-main/timeline/baumquist-joins-digitall-as-cont") {
+                    title
+                }
+            }`
+        }, axiosConf);
+
+        const { data } = response;
+
+        expect(data.errors[0]).toHaveProperty("description", "Field 'newsSDLByPath' in type 'Query' is undefined");
+    });
+
+
+});

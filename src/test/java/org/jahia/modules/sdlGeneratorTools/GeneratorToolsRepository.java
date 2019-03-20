f98bda5112d664f47b3f3005d998453649a5553f
astritademi <aademi@jahia.com>
2019-03-20 14:31:08 -0400

    Updated addAProperty method

--- src/test/java/org/jahia/modules/sdlGeneratorTools/GeneratorToolsRepository.java
+++ src/test/java/org/jahia/modules/sdlGeneratorTools/GeneratorToolsRepository.java
@@ -13,6 +13,8 @@ public class GeneratorToolsRepository extends GqlApiController {
 
         clickOn(addNewTypeBtn);
 
+        checkCreateTypeDialog();
+
         selectNodeType(nodeType, typeName);
 
         findByXpath("//input[@id='typeName']").sendKeys(typeName);
@@ -56,4 +58,131 @@ public class GeneratorToolsRepository extends GqlApiController {
                 {"jnt:content","content", 30}
         };
     }
+
+    protected void addAProperty(String property, String propertyName) {
+        clickOn(findByXpath("//span/p[contains(., 'Add new property')]"));
+
+        waitForElementToBeClickable(findByXpath("//p[contains(.,'Select property')]/parent::span/parent::button"));
+        clickOn(findByXpath("//p[contains(.,'Select property')]/parent::span/parent::button"));
+        shortSleep();
+
+        checkAddPropertyDialog();
+
+        clickOn(findByXpath("//div[contains(@class, 'MuiSelect-selectMenu-')]"));
+        shortSleep();
+
+        clickOn(findByXpath("//li[@data-value='"+property+"']"));
+
+        findByXpath("//input[@id='propertyName']").sendKeys(propertyName);
+
+        clickOn(findByXpath("//span/p[contains(.,'Save')]"));
+    }
+
+    protected void addAFinder(String finder, String customName) {
+        waitForElementToBeVisible(findByXpath("//p[contains(.,'Add a finder')]"));
+
+        clickOn(findByXpath("//p[contains(.,'Add a finder')]"));
+
+        checkAddFinderDialog();
+
+        clickOn(findByXpath("//input[@id='finder-name']/parent::div/div"));
+
+        waitForElementToBeVisible(findByXpath("//ul[@role='listbox']"));
+
+        clickOn(findByXpath("//li[@data-value='"+finder+"']"));
+
+        findByXpath("//input[@id='propertyName']").sendKeys(customName);
+
+        if (finder.equals("all")) {
+            Assert.assertEquals(findByXpath("//p[contains(@class,'FinderPreviewComp')]/span[1]").getText(), finder,
+                    "the finder preview is incorrect");
+            String partialFinderPreview = findByXpath("//p[contains(@class,'FinderPreviewComp')]/em").getText().toLowerCase();
+            Assert.assertEquals(partialFinderPreview, customName,
+                    "the finder preview is incorrect");
+        } else if (finder.equals("allConnection")) {
+            Assert.assertEquals(findByXpath("//p[contains(@class,'FinderPreviewComp')]/span[1]").getText(), "all",
+                    "the finder preview is incorrect");
+            String partialFinderPreview = findByXpath("//p[contains(@class,'FinderPreviewComp')]/em").getText().toLowerCase();
+            Assert.assertEquals(partialFinderPreview, customName,
+                    "the finder preview is incorrect");
+            Assert.assertEquals(findByXpath("//p[contains(@class,'FinderPreviewComp')]/span[2]").getText(), "Connection",
+                    "the finder preview is incorrect");
+        } else {
+            String partialFinderPreview = findByXpath("//p[contains(@class,'FinderPreviewComp')]/em").getText().toLowerCase();
+            Assert.assertEquals(partialFinderPreview, customName,
+                    "the finder preview is incorrect");
+            Assert.assertEquals(findByXpath("//p[contains(@class,'FinderPreviewComp')]/span").getText(), finder,
+                    "the finder preview is incorrect");
+        }
+
+        clickOn(findByXpath("//p[contains(.,'Save')]/parent::span/parent::button"));
+    }
+
+    protected void checkAddPropertyDialog() {
+        Assert.assertTrue(findByXpath("//h2[contains(text(),'Add new property')]").isDisplayed(),
+                "Add new property dialog box failed to open");
+        Assert.assertTrue(findByXpath("//p[contains(text(),'Select a property')]").isDisplayed(),
+                "Add new property dialog box failed to open");
+        Assert.assertTrue(findByXpath("//div[contains(@class,'MuiSelect-selectMenu-')]").isDisplayed(),
+                "Add new property dialog box failed to open");
+        Assert.assertTrue(findByXpath("//p[contains(text(),'Custom property name')]").isDisplayed(),
+                "Add new property dialog box failed to open");
+        Assert.assertTrue(findByXpath("//input[@id='propertyName']").isDisplayed(),
+                "Add new property dialog box failed to open");
+        Assert.assertTrue(findByXpath("//p[contains(., 'Cancel')]/parent::span/parent::button").isDisplayed(),
+                "Add property dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//p[contains(., 'Save')]/parent::span/parent::button").isDisplayed(),
+                "Add property dialog box failed to load as expected");
+    }
+
+    protected void checkCreateTypeDialog() {
+        Assert.assertTrue(findByXpath("//h2[contains(., 'Add new type')]").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findElementsByXpath("//div[contains(.,'Select or search a node')]").get(9).isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//label/p[contains(., 'Custom type name')]").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//input[@id='typeName']").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//p[contains(., 'Remove ByPath and ById entry points')]").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//p[contains(., 'Cancel')]/parent::span/parent::button").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//p[contains(., 'Save')]/parent::span/parent::button").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+    }
+
+    protected void checkEditTypeDialog() {
+        Assert.assertTrue(findByXpath("//h2[contains(., 'Edit type')]").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//label[contains(., 'Select a node type')]").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//label[contains(., 'Custom type name')]").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//input[@id='typeName']").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//span[contains(., 'Ignore Default Queries')]").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//p[contains(., 'Cancel')]/parent::span/parent::button").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+        Assert.assertTrue(findByXpath("//p[contains(., 'Save')]/parent::span/parent::button").isDisplayed(),
+                "Add new type dialog box failed to load as expected");
+    }
+
+    protected void checkAddFinderDialog() {
+        waitForElementToBeVisible(findByID("form-dialog-title"));
+        Assert.assertTrue(findByXpath("//p[contains(.,'Select a finder')]").isDisplayed(),
+                "Add finder dialog failed to load correctly");
+        Assert.assertTrue(findByXpath("//input[@id='finder-name']/parent::div/div").isDisplayed(),
+                "Select a finder input field is not visible");
+        Assert.assertTrue(findByXpath("//p[contains(.,'Custom name')]").isDisplayed(),
+                "title for Custom name input is not visible");
+        Assert.assertTrue(findByXpath("//input[@id='propertyName']").isDisplayed(),
+                "Custom name input field is not visible");
+        Assert.assertTrue(findByXpath("//p[contains(.,'Cancel')]/parent::span/parent::button").isDisplayed(),
+                "Cancel button is not visible");
+        Assert.assertTrue(findByXpath("//p[contains(.,'Save')]/parent::span/parent::button").isDisplayed(),
+                "Save button is not visible");
+    }
+
 }

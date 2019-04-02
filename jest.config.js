455b9c4a3e2650e7697b4b652c28928242ccc2b9
cedmail <cmailleux@jahia.com>
2019-04-02 12:35:20 -0400

    activate test-results-processor in config file

--- jest.config.js
+++ jest.config.js
@@ -148,7 +148,7 @@ module.exports = {
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: null,
-
+"testResultsProcessor": "jest-teamcity-reporter",
   // This option allows use of a custom test runner
   // testRunner: "jasmine2",
 
